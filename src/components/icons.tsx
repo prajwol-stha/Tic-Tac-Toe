534c12ac8b6fa8ad6de0918b99eafbfd3f948885
Prajwol Shrestha <prajwol.stha777@gmail.com>
2024-02-02 13:34:46 +0545

    completed

---
+++ src/components/icons.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import type {PropsWithChildren} from 'react';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+type IconsProps = PropsWithChildren<{
+  name: string;
+}>;
+
+const Icons = ({name}: IconsProps) => {
+  switch (name) {
+    case 'circle':
+      return <Icon name="circle-thin" size={38} color="#F7CD2E" />;
+      break;
+
+    case 'cross':
+      return <Icon name="times" size={38} color="#38CC77" />;
+      break;
+
+    default:
+      return <Icon name="pencil" size={38} color="#567676" />;
+  }
+  return <div>icons</div>;
+};
+
+export default Icons;
